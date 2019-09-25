23141a83c9e184f7105ff6f3d9f0c9d811b17b25
Cody Ridge <cr679717@ohio.edu>
2019-09-25 17:36:14 -0400

    MapBox API files

---
+++ Indoor GPS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Indoor GPS
+//
+//  Created by Cody Nathanael Ridge on 9/17/19.
+//  Copyright © 2019 Cody Nathanael Ridge. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
