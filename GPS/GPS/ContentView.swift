a14e33a25494c1b67f39c7c5cc533c4984609196
Dylan <dylansmith@Dylans-MacBook-Pro-2.local>
2019-10-01 17:30:48 -0400

    I just added the UI

---
+++ GPS/GPS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GPS
+//
+//  Created by Dylan Smith on 10/1/19.
+//  Copyright © 2019 Dylan Smith. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
